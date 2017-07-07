0c29f3d0a3bc9bc2b38cac8813475c708e24f7b9
Stanlee Okwii <stanleeparker12@gmail.com>
2017-07-07 13:07:38 +0300

    New code has arrived

--- functions.js
+++ functions.js
@@ -1,7 +1,20 @@
-var Addarray = [2,4,5,6,7,8,9];
-function add(a, b){
-    return a+b;
+
+function add(Addarray){
+    Addarray = [];
+    var result = 0;
+    for(var n = 0;Addarray.length < n; n += 1 ){
+        result += Addarray[n];
+    };
+    return result;
 }
+
+var AddMyArray = function () {  
+      var i, sum = 0;  
+      for (i = 0; i < arguments.length; i += 1){
+           sum += arguments[i];   
+        } 
+      return sum;
+     }
 
 function sub(a, b){
     return a-b;
@@ -14,5 +27,5 @@ function mul(a, b){
 function div(a, b){
     return a/b;
 }
-console.log(add(3,4));
-
+console.log(add(3,4,3,4,45,5));
+console.log(AddMyArray(3,4,3,4,45,5));
